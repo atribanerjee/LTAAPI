ac814e99f0187dcff3ec129caa28e6cb72d8c138
atribanerjee <atri.tih@gmail.com>
2024-04-16 17:22:08 +0530

    modify swagger

--- LTAAPI/Program.cs
+++ LTAAPI/Program.cs
@@ -58,11 +58,16 @@ builder.Services.AddScoped<IJWTRepository,JWTService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
