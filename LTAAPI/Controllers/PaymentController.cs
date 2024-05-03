eb7c33ea9afb36d36a90efcd88671fc26f73f3dd
atribanerjee <atri.tih@gmail.com>
2024-05-03 20:44:28 +0530

    update payment controller

---
+++ LTAAPI/Controllers/PaymentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LTAAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentController : Controller
+    {
+        [Route("success")]
+        [HttpPost]
+        public IActionResult HandleSuccess()
+        {
+
+            return Ok(new { message = "Payment successful" });
+        }
+
+
+
+       
+    }
+}
