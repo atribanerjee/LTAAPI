14af3bfd4e457f76da9bf6adfc3344c3e068ca80
atribanerjee <atri.tih@gmail.com>
2024-06-05 15:20:53 +0530

    Hash Password encrypted

--- LTAAPI/Services/AuthService.cs
+++ LTAAPI/Services/AuthService.cs
@@ -7,6 +7,8 @@ using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
 using static LTAAPI.Services.AuthService;
+using BCrypt.Net;
+using Microsoft.IdentityModel.Tokens;
 
 namespace LTAAPI.Services
 {
@@ -78,78 +80,63 @@ namespace LTAAPI.Services
 
         
         //-------------------------------
-        //public class PasswordService
+        
+        //public string Encrypt(string clearText)
         //{
-        //    private readonly IPasswordHasher<IdentityUser> _passwordHasher;
-
-        //    public PasswordService(IPasswordHasher<IdentityUser> passwordHasher)
+        //    byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+        //    using (Aes encryptor = Aes.Create())
         //    {
-        //        _passwordHasher = passwordHasher;
+        //        Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes("true", new byte[] { 0x65, 0x3d, 0x54, 0x9d, 0x76, 0x49, 0x76, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x61 });
+        //        encryptor.Key = pdb.GetBytes(32);
+        //        encryptor.IV = pdb.GetBytes(16);
+        //        using (MemoryStream ms = new MemoryStream())
+        //        {
+        //            using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+        //            {
+        //                cs.Write(clearBytes, 0, clearBytes.Length);
+        //                cs.Close();
+        //            }
+        //            clearText = Convert.ToBase64String(ms.ToArray());
+        //        }
         //    }
-
-        //    public string HashPassword(string password)
-        //    {
-        //        var user = new IdentityUser();
-        //        return _passwordHasher.HashPassword(user, password);
-        //    }
-
-        //    public PasswordVerificationResult VerifyPassword(string hashedPassword, string providedPassword)
-        //    {
-        //        var user = new IdentityUser();
-        //        return _passwordHasher.VerifyHashedPassword(user, hashedPassword, providedPassword);
-        //    }
+        //    return clearText;
         //}
-        //private readonly IPasswordHasher<IdentityUser> _passwordHasher;
-        //private readonly PasswordService _passwordService;
-
-        public string Encrypt(string clearText)
-        {
-            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
-            using (Aes encryptor = Aes.Create())
-            {
-                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes("true", new byte[] { 0x65, 0x3d, 0x54, 0x9d, 0x76, 0x49, 0x76, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x61 });
-                encryptor.Key = pdb.GetBytes(32);
-                encryptor.IV = pdb.GetBytes(16);
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
-                    {
-                        cs.Write(clearBytes, 0, clearBytes.Length);
-                        cs.Close();
-                    }
-                    clearText = Convert.ToBase64String(ms.ToArray());
-                }
-            }
-            return clearText;
-        }
 
         //--------------------------------
         public async Task<UsersModel> UserLogin(LoginModel loginModel)
         {
             UsersModel? ReturnModel = new UsersModel();
             try
-            {
-                //-----------------------------
-                //var hashedPassword = _passwordService.HashPassword(loginModel.Password);
-                var hashedPassword = Encrypt(loginModel.Password);
-                //-----------------------------
-                if (loginModel != null && !String.IsNullOrEmpty(loginModel.Email) && !String.IsNullOrEmpty(hashedPassword)) //loginModel.Password))
+            {                
+                if (loginModel != null && !String.IsNullOrEmpty(loginModel.Email) && !String.IsNullOrEmpty(loginModel.Password))
                 {
-                    ReturnModel = await (from u in _context.Users
-                                         where u.Email == loginModel.Email
-                                         && u.Password == hashedPassword //loginModel.Password
-                                         select new UsersModel
-                                         {
-                                             ID = u.ID,
-                                             UserName = u.UserName,
-                                             Email = u.Email
+                    //---------------------------------------------
 
-                                         }).FirstOrDefaultAsync();
+                    var user = _context.Users.FirstOrDefault(u => u.Email == loginModel.Email);
+                    if (user != null && !string.IsNullOrEmpty(user.Password))
+                    {
+                        string hashedPasswordFromDatabase = user.Password;
+                        bool passwordMatches = BCrypt.Net.BCrypt.Verify(loginModel.Password, hashedPasswordFromDatabase);
+                        if (passwordMatches)
+                        {
+                            //--------------------------------------------
+                            ReturnModel = await (from u in _context.Users
+                                                 where u.Email == loginModel.Email
+                                                 && u.Password == hashedPasswordFromDatabase //loginModel.Password
+                                                 select new UsersModel
+                                                 {
+                                                     ID = u.ID,
+                                                     UserName = u.UserName,
+                                                     Email = u.Email
+
+                                                 }).FirstOrDefaultAsync();
+                        }
+                    }
                 }
             }
             catch (Exception Ex)
             {
-
+                
             }
 
             return ReturnModel;
@@ -161,9 +148,8 @@ namespace LTAAPI.Services
             try
             {
                 //----------------------------------
-                var hashedPassword = Encrypt(model.Password);
-                //this._passwordHasher = model.Password;
-                //var hashedPassword = _passwordService.HashPassword(model.Password);
+                //var hashedPassword = Encrypt(model.Password);
+                var hashedPassword = BCrypt.Net.BCrypt.HashPassword(model.Password);                
                 //-----------------------------------
                 if (model != null && !String.IsNullOrEmpty(model.UserName) && !String.IsNullOrEmpty(model.Password) && !String.IsNullOrEmpty(model.Email))
                 {
